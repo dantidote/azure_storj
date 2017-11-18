820496ef3f89c4d12906d94646510f111c392074
Dan DeWaele <dantidote@gmail.com>
2017-11-18 08:26:40 -0500

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name 'azure-storj'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures azure-storj'
+long_description 'Installs/Configures azure-storj'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/azure-storj/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/azure-storj'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: azure-storj
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
